d63623ab98bdaada293104a38bfdf2ba36b1f8a1
Marco Galizzi <marcogalizzi89@gmail.com>
2018-12-18 21:50:20 +0100

    bug

--- router.js
+++ router.js
@@ -1,6 +1,6 @@
 const Router = require('koa-router');
 const router = new Router();
 
-router.get('/', ctx => (ctx.body = 'hello'));
+router.get('/hello', ctx => (ctx.body = 'hello'));
 
 module.exports = router;
