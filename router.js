dc71e5bdf0e5a85db85033daca45297bbb6a616a
Marco Galizzi <marcogalizzi89@gmail.com>
2018-12-18 22:05:15 +0100

    bug

--- router.js
+++ router.js
@@ -1,6 +1,19 @@
 const Router = require('koa-router');
 const router = new Router();
 
-router.get('/hello', ctx => (ctx.body = 'hello'));
+const storage = [];
+
+router.post('/makejson', ctx => {
+  const file = ctx.request.body;
+  storage.push(file);
+  console.log(storage);
+  ctx.body = { ok: 'ok' };
+});
+
+router.get('/download', ctx => {
+  const file = storage[0];
+  ctx.attachment(file);
+  storage.splice(0, 1);
+});
 
 module.exports = router;
